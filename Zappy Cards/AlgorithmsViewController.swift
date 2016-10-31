aa6cf360c85b7855604f911b3fe99e6194f75618
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2016-10-30 17:29:58 -0700

    finished tableview details for the three new panels.

--- Zappy Cards/AlgorithmsViewController.swift
+++ Zappy Cards/AlgorithmsViewController.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 
-class AlgorithmsViewController: UIViewController {
+class AlgorithmsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "hello"
+        return cell
+    }
 
 }
--- Zappy Cards/InterviewViewController.swift
+++ Zappy Cards/InterviewViewController.swift
@@ -8,28 +8,28 @@
 
 import UIKit
 
-class InterviewViewController: UIViewController {
+class InterviewViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "hello"
+        return cell
     }
-    */
 
 }
