7b91430c50d7241bdaefe75e6a575a4aa6aa2808
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2016-11-12 18:44:55 -0800

    added more questions and answers.

--- Zappy Cards/InterviewViewController.swift
+++ Zappy Cards/InterviewViewController.swift
@@ -52,20 +52,69 @@ class InterviewViewController: UIViewController, UITableViewDataSource, UITableV
     func makeQuizArray() -> [Quiz] {
         let quizI1 = Quiz()
         
-        quizI1.question = "Q1: What is the value of len? var array1 = [1, 2, 3, 4, 5]; var array2 = array1; array2.append(6); var len = array1.count."
-        quizI1.answer = "The len variable is equal to 5, meaning that array1 has 5 elements, whereas array2 has 6 elements. When array1 is assigned to array2, a copy of array1 is actually created and assigned. The reason is that swift arrays are value types (implemented as structs) and not reference types (i.e. classes). When a value type is assigned to a variable, passed as argument to a function or method, or otherwise moved around, a copy of it is actually created and assigned or passed. Note that swift dictionaries are also value types, implemented as structs."
+        quizI1.question = "Q1: What is the value of len?" + "\n\n" + "var array1 = [1, 2, 3, 4, 5];" + "\n\n" + "var array2 = array1; array2.append(6);" + "\n\n" + "var len = array1.count."
+        quizI1.answer = "A1: The len variable is equal to 5, meaning that array1 has 5 elements, whereas array2 has 6 elements. When array1 is assigned to array2, a copy of array1 is actually created and assigned. The reason is that swift arrays are value types (implemented as structs) and not reference types (i.e. classes)." + "\n\n" + "When a value type is assigned to a variable, passed as argument to a function or method, or otherwise moved around, a copy of it is actually created and assigned or passed. Note that swift dictionaries are also value types, implemented as structs."
         quizI1.category = "Interview Questions"
         quizI1.author = "Rohit"
         quizI1.number = 1
         
         let quizI2 = Quiz()
         quizI2.question = "Q2: In Swift enumerations, what’s the difference between raw values and associated values?"
-        quizI2.answer = "Raw values are used to associate constant (literal) values to enum cases. The value type is part of the enum type, and each enum case must specify a unique raw value (duplicate values are not allowed)."
+        quizI2.answer = "A2: Raw values are used to associate constant (literal) values to enum cases. The value type is part of the enum type, and each enum case must specify a unique raw value (duplicate values are not allowed)."
         quizI2.category = "Interview Questions"
         quizI2.author = "Rohit"
         quizI2.number = 2
         
-        return [quizI1, quizI2]
+        let quizI3 = Quiz()
+        quizI3.question = "Q3: Consider the following" + "\n\n" + "var optional1: String? = nil" + "\n\n" + "var optional2: String? = .None" + "\n\n" + "What’s the difference between nil and .None? How do the optional1 and optional2 variables differ?"
+        quizI3.answer = "A3: There is no difference. Optional.None (.None for short) is the correct way of initializing an optional variable lacking a value, whereas nil is just syntactic sugar for .None." + "\n\n" + "In fact, this statement outputs true:" + "\n\n" + "nil == .None // On Swift 1.x this doesn't compile. You need Optional<Int>.None"
+        quizI3.category = "Interview Questions"
+        quizI3.author = "Rohit"
+        quizI3.number = 3
+        
+        let quizI4 = Quiz()
+        quizI4.question = "Q4: Explain what is optional chaining."
+        quizI4.answer = "A4: Optional chaining is a process of querying and calling properties. Multiple queries can be chained together, and if any link in the chain is nil then, the entire chain fails." + "\n\n" + "Ex: let candname = cand.candidate!.name"
+        quizI4.category = "Interview Questions"
+        quizI4.author = "Rohit"
+        quizI4.number = 4
+        
+        let quizI5 = Quiz()
+        quizI5.question = "Q5: Explain what Lazy stored properties is and when it is useful."
+        quizI5.answer = "A4: Lazy stored properties are used for a property whose initial values is not calculated until the first time it is used.  You can declare a lazy stored property by writing the lazy modifier before its declaration. Lazy properties are useful when the initial value for a property is reliant on outside factors whose values are unknown." + "\n\n" + "lazy var importer = DataImporter()"
+        quizI5.category = "Interview Questions"
+        quizI5.author = "Rohit"
+        quizI5.number = 5
+        
+        let quizI6 = Quiz()
+        quizI6.question = "Q6: What are extensions?"
+        quizI6.answer = "A6: Extensions add new functionality to an existing class, structure, enumeration, or protocol type. This includes the ability to extend types for which you do not have access to the original source code." + "\n\n" + "extension Double {var km: Double { return self * 1_000.0 }}"
+        quizI6.category = "Interview Questions"
+        quizI6.author = "Rohit"
+        quizI6.number = 6
+        
+        let quizI7 = Quiz()
+        quizI7.question = "Q7: Explain the difference between extension and subclassing."
+        quizI7.answer = "A7: Ex: UITextField. If you are adding general-purpose functionalities that should be available to every UITextField, then use extension." + "\n\n" + "If you are adding functionality that should be restricted to special instances of UITextField, then use a subclass."
+        quizI7.category = "Interview Questions"
+        quizI7.author = "Rohit"
+        quizI7.number = 7
+        
+        let quizI8 = Quiz()
+        quizI8.question = "Q8: What is a protocol?"
+        quizI8.answer = "A8: Protocols are similar to interfaces in other languages. A protocol specifies a set of behaviours that something should implement, without providing the implementation for those behaviours."
+        quizI8.category = "Interview Questions"
+        quizI8.author = "Rohit"
+        quizI8.number = 8
+        
+        let quizI9 = Quiz()
+        quizI9.question = "Q9: Compare Protocol in Swift vs Interface in Java."
+        quizI9.answer = "A9: Swift protocols can also specify properties that must be implemented (i.e. fields)" + "\n\n" + "Swift protocols need to deal with value/reference through the use of the mutating keyword (because protocols can be implemented by structs and classes)" + "\n\n" + "you can combine protocols at any point with the protocol<> keyword. For example, declaring a function parameter that must adhere to protocol A and B as:" + "\n\n" + "func foo ( var1 : protocol<A, B> ){}"
+        quizI9.category = "Interview Questions"
+        quizI9.author = "Rohit"
+        quizI9.number = 9
+        
+        return [quizI1, quizI2, quizI3, quizI4, quizI5, quizI6, quizI7, quizI8]
     }
 
 }
--- Zappy Cards/MethodsViewController.swift
+++ Zappy Cards/MethodsViewController.swift
@@ -52,20 +52,55 @@ class MethodsViewController: UIViewController, UITableViewDataSource, UITableVie
     
         let quizM1 = Quiz()
         
-        quizM1.question = "Q1: What is meant by numberOfRowsInSection in tableView?"
-        quizM1.answer = "Swift introduces optional types, which handle the absence of a value. Optionals say either “there is a value, and it equals x” or “there isn’t a value at all”. Using optionals is similar to using nil with pointers in Objective-C, but they work for any type, not just classes. Not only are optionals safer and more expressive than nil pointers in Objective-C, they are at the heart of many of Swift’s most powerful features."
-        quizM1.category = "Swift Concepts"
+        quizM1.question = "Q1: What are key methods for manipulating images?"
+        quizM1.answer = "A1: func UIImageJPEGRepresentation(UIImage, CGFloat)." + "\n\n" + "UIImagePNGRepresentation(UIImage)"
+        quizM1.category = "Xcode Methods"
         quizM1.author = "Rohit"
         quizM1.number = 1
         
         let quizM2 = Quiz()
-        quizM2.question = "Q2: What does  didSelectRowAt indexPath in tableView do?"
-        quizM2.answer = "Swift is a type-safe language, which means the language helps you to be clear about the types of values your code can work with. If part of your code expects a String, type safety prevents you from passing it an Int by mistake. Likewise, type safety prevents you from accidentally passing an optional String to a piece of code that expects a nonoptional String. Type safety helps you catch and fix errors as early as possible in the development process."
-        quizM2.category = "Swift Concepts"
+        quizM2.question = "Q2: Name three tableview datasource methods."
+        quizM2.answer = "A2: func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int" + "\n\n" + "func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell" + "\n\n" + "(if using lots of data) func numberOfSectionsInTableView(tableView: UITableView) -> Int"
+        quizM2.category = "Xcode Methods"
         quizM2.author = "Rohit"
         quizM2.number = 2
         
-        return [quizM1, quizM2]
+        let quizM3 = Quiz()
+        quizM3.question = "Q3: What does tableView(_:cellForRowAtIndexPath:) do?"
+        quizM3.answer = "A3: It configures and provides a cell to display for a given row. ." + "\n\n" + "For table views with a large number of rows it displays only a small fraction of their total items at a given time. It’s most efficient for table views to only ask for the cells for rows that are being displayed."
+        quizM3.category = "Xcode Methods"
+        quizM3.author = "Rohit"
+        quizM3.number = 3
+        
+        let quizM4 = Quiz()
+        quizM4.question = "Q4: What does tableView(_:numberOfRowsInSection:) do?"
+        quizM4.answer = "A4: It tells the table view how many rows to display in a given section."
+        quizM4.category = "Xcode Methods"
+        quizM4.author = "Rohit"
+        quizM4.number = 4
+        
+        let quizM5 = Quiz()
+        quizM5.question = "Q5: What is the code to show user location in Mapkit?"
+        quizM5.answer = "A5: mapView.showsUserLocation = true."
+        quizM5.category = "Xcode Methods"
+        quizM5.author = "Rohit"
+        quizM5.number = 5
+        
+        let quizM6 = Quiz()
+        quizM6.question = "Q6: What is CLLocationManager?"
+        quizM6.answer = "A6: The CLLocationManager is responsible for tracking where the user is and anytime there is an update that the User moved from one place to another." + "\n\n" + "Usage: var manager = CLLocationManager()"
+        quizM6.category = "Xcode Methods"
+        quizM6.author = "Rohit"
+        quizM6.number = 6
+        
+        let quizM7 = Quiz()
+        quizM7.question = "Q6: List key methods/steps to enable Mapviews"
+        quizM7.answer = "A6: var manager = CLLocationManager()" + "\n\n" + "In viewDidLoad: manager.delegate = self" + "\n\n" + "Add to View Controller, a CLLocationManagerDelegate"
+        quizM7.category = "Xcode Methods" + "\n\n" + "mapview.showsUserLocation = true" + "\n\n" + "manager.requestWhenInUseAuthorization"
+        quizM7.author = "Rohit"
+        quizM7.number = 7
+        
+        return [quizM1, quizM2, quizM3, quizM4, quizM5, quizM6, quizM7]
     }
 
 }
